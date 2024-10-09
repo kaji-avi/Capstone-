05c1839daff27a525cb6ec64a221ae7297688326
Mutuwira <92231535+Mutuwira@users.noreply.github.com>
2024-10-09 11:50:25 +0400

    update

---
+++ channelEmulation.m
@@ -0,0 +1,12 @@
+function y = channelEmulation(x,snr,to,h)
+
+y = zeros(1,to); % create some offset
+y = [y conv(h,x)];
+
+% scale with SNR and multiply CFO
+y = sqrt(snr).*y;
+
+% add awgn
+y = y+sqrt(1/2).*(randn(size(y))+1i.*randn(size(y)));
+
+end
